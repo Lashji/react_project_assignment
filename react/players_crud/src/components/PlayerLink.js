a771126a33f316db63b1467fb7d0d09a93683240
Lassi <lassi.palojarvi@outlook.com>
2022-01-30 23:30:17 +0200

    fix for onClick?

--- react/players_crud/src/components/PlayerLink.js
+++ react/players_crud/src/components/PlayerLink.js
@@ -1,10 +1,3 @@
 export const PlayerLink = ({ name, onClick, url }) => {
-
-  const test = (e, url) => {
-    console.log("onCLick clicked")
-
-    onClick(e, url)
-  }
-
-  return <a href={url} onClick={e => test(e, url)}>{name}</a>;
+  return <a href={url} onClick={e => onClick(e, url)}>{name}</a>;
 };
