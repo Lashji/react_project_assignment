5ea49d7ec56fc52c6405e5079e1e51784405bd48
Lassi <lassi.palojarvi@outlook.com>
2022-02-03 00:59:16 +0200

    init

--- redux/pure_redux/actionCreators.js
+++ redux/pure_redux/actionCreators.js
@@ -2,15 +2,26 @@ import * as actions from './actionTypes.js';
 
 export const addPlayer = (name, isActive = false) => {
   // TODO: Implement this function
-  throw new Error('Not Implemented');
+  return {
+    type: actions.ADD_PLAYER,
+    payload: {name, isActive}
+  }
 };
 
 export const removePlayer = id => {
-  // TODO: Implement this function
-  throw new Error('Not Implemented');
+  return {
+    type: actions.REMOVE_PLAYER,
+    payload: {
+     id
+    }
+  }
 };
 
 export const togglePlayerStatus = id => {
-  // TODO: Implement this function
-  throw new Error('Not Implemented');
+  return {
+    type: actions.TOGGLE_PLAYER_STATUS,
+    payload: {
+      id
+    }
+  }
 };
--- redux/pure_redux/reducer.js
+++ redux/pure_redux/reducer.js
@@ -1,6 +1,32 @@
 import * as actions from './actionTypes.js';
 
 export default (state = [], action) => {
-  // TODO: Implement this function
-  throw new Error('Not Implemented');
+
+  switch (action.type) {
+    case actions.ADD_PLAYER:
+
+
+      const player = {
+        ...action.payload,
+        id: state.length +1 
+      }
+
+      return [...state, player]
+      
+    case actions.REMOVE_PLAYER:
+      return [...state].filter(i => i.id !== action.payload.id)
+     
+    case actions.TOGGLE_PLAYER_STATUS:
+      return [...state].map(i => {
+          if (i.id === action.payload.id) {
+            i.isActive = !i.isActive
+        }
+          return i
+        })
+      
+    
+    default:
+      return state
+  }
+
 };
