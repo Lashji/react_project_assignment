b87744cbb3bd60c17be23e133498b27e1d9ffb66
Lassi <lassi.palojarvi@outlook.com>
2022-03-06 18:14:11 +0200

    Added Routes

--- assignment/frontend/src/App.jsx
+++ assignment/frontend/src/App.jsx
@@ -1,36 +1,49 @@
 /** @format */
-import { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
-import { Route, Routes } from 'react-router';
-import Cart from './components/Cart';
-import Home from './components/Home';
-import Login from './components/Login';
-import Navbar from './components/Navbar';
-import NotFound from './components/NotFound';
-import Notification from './components/Notification';
-import Register from './components/Register';
-import User from './components/User';
-import UserModifier from './components/UserModifier';
-import Users from './components/Users';
-import Auth from './components/Auth';
-import Order from './components/Order';
-import Orders from './components/Orders';
-import Products from './components/Products';
-import Product from './components/Product';
-import ProductModifier from './components/ProductModifier';
-import Finder from './components/Finder';
-import { getOrder } from './redux/actionCreators/ordersActions';
-import { getProduct } from './redux/actionCreators/productsActions';
-import { getUser } from './redux/actionCreators/usersActions';
-import { initApp } from './redux/actionCreators/appActions';
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { Route, Routes } from "react-router";
+import Cart from "./components/Cart";
+import Home from "./components/Home";
+import Login from "./components/Login";
+import Navbar from "./components/Navbar";
+import NotFound from "./components/NotFound";
+import Notification from "./components/Notification";
+import Register from "./components/Register";
+import User from "./components/User";
+import UserModifier from "./components/UserModifier";
+import Users from "./components/Users";
+import Auth from "./components/Auth";
+import Order from "./components/Order";
+import Orders from "./components/Orders";
+import Products from "./components/Products";
+import Product from "./components/Product";
+import ProductModifier from "./components/ProductModifier";
+import Finder from "./components/Finder";
+import { getOrder } from "./redux/actionCreators/ordersActions";
+import { getProduct } from "./redux/actionCreators/productsActions";
+import { getUser } from "./redux/actionCreators/usersActions";
+import { initApp } from "./redux/actionCreators/appActions";
+
 const App = () => {
-	return (
-		<div data-testid='app-component'>
-			<footer>
-				<p>Copyright &copy; 2022</p>
-			</footer>
-		</div>
-	);
+  return (
+    <Routes>
+      <Route path="/" element={<Home />}/>
+      <Route path="/products" element={<Products />} />
+      <Route path="/products/:productId" element={<Product />} />
+      <Route
+        path="/products/:productId/modify"
+        element={<ProductModifier />}
+      />
+      <Route path="/cart" element={<Cart />}/>
+      <Route path="/orders" element={<Orders />} />
+      <Route path="/orders/:orderId" element={<Order} />
+      <Route path="/register" element={ <Register />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/users" element={ <Users />} />
+      <Route path="/users/:userId" element={ <User /> } />
+      <Route path="/users/:userId/modify" element={ <UserModifier />} /> 
+    </Routes>
+  );
 };
 
 export default App;
