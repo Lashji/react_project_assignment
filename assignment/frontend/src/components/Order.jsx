2b750ccef613d84976e21c8833dce0e3f298ae34
Lassi <lassi.palojarvi@outlook.com>
2022-03-08 01:18:49 +0200

    orderItem

--- assignment/frontend/src/components/Order.jsx
+++ assignment/frontend/src/components/Order.jsx
@@ -5,7 +5,27 @@ import { useSelector } from "react-redux";
 import { Link, useParams } from "react-router-dom";
 
 const Order = ({ providedOrder }) => {
-  return <div></div>;
+  const { id } = useParams();
+
+  const orderItems = providedOrder.items.map((i) => {
+    return (
+      <li data-testid="order-listitem">
+        <h4 data-testid="name-heading">{i.name}</h4>
+        <div data-testid="price-element">{i.price}</div>
+        <div data-testid="description-element">{i.description}</div>
+        <div data-testid="quantity-element">{i.quantity}</div>
+      </li>
+    );
+  });
+
+  return (
+    <div data-testid="order-component">
+      <div data-testid="order-id"></div>
+      <div data-testid="order-customer-id"></div>
+      <Link to={`./orders/${id}`} />
+      <ol>{orderItems}</ol>
+    </div>
+  );
 };
 
 export default Order;
